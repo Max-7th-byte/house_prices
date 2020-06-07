34e0b66bc8dcf3e6c3d366847261330cb867a65d
Max-7th-byte <max.dubakov@gmail.com>
2020-06-07 15:10:55 +0200

    Base is finished

--- launch.m
+++ launch.m
@@ -20,11 +20,20 @@ fprintf('Theta vector is: \n');
 theta
 fprintf('Program paused. Press enter to continue.\n');
 pause;
-u_ans = input('Do you want to see the graph of cost function?(y/n) ');
-if u_ans == 'y'
+u_ans = input('Do you want to see the graph of cost function?(1 for yes/0 for no) ');
+if u_ans == 1
+  figure(1);
   plot(1:numel(J_history), J_history, '-r', 'LineWidth', 2);
 end
 
 fprintf('Please, enter the parameters of your house:\n');
 size = input('Size (feet squared): ');
 bedrooms = input('Number of bedrooms:  ');
+params = [1 size bedrooms];
+for i=2:3
+  params(i) = (params(i) - mu(i-1))*(1/sigma(i-1));
+end
+
+price = params * theta;
+fprintf(['Predicted price of the house ' ...
+         ':\n $%f\n'], ceil(price));
